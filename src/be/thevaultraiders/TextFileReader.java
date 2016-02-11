410d1052d6a4dabc2ac368cd7bc2d8992b53a204
Azer1652 <arthurjanssens@hotmail.com>
2016-02-11 20:34:56 +0100

    Text Loading Almost Done

--- src/be/thevaultraiders/TextFileReader.java
+++ src/be/thevaultraiders/TextFileReader.java
@@ -4,14 +4,20 @@ package be.thevaultraiders;
  * Created by Arthur on 11/02/2016.
  */
 
+import be.thevaultraiders.models.Product;
+import be.thevaultraiders.models.Warehouse;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextFileReader {
 
     public static void main(String[] args){
-        new TextFileReader();
+        new TextFileReader("files/busy_day.in");
     }
 
     private BufferedReader reader;
@@ -27,19 +33,37 @@ public class TextFileReader {
             sCurrentLine = reader.readLine();
             line++;
             //Read First Line
-            String sCurrLineSplit[] = sCurrentLine.split();
+            String sCurrLineSplit[] = sCurrentLine.split(" ");
+            System.out.println(sCurrLineSplit[0]);
+            System.out.println(sCurrLineSplit[1]);
+            System.out.println(sCurrLineSplit.length);
+
+            int nRows = Integer.parseInt(sCurrLineSplit[0]);
+            int nCols = Integer.parseInt(sCurrLineSplit[1]);
+            int nDrones = Integer.parseInt(sCurrLineSplit[2]);
+            int tDeadline = Integer.parseInt(sCurrLineSplit[3]);
+            int maxLoad = Integer.parseInt(sCurrLineSplit[4]);
+
+            int[] weights = parseWeights();
+
+            sCurrentLine = reader.readLine();
+            line++;
+            int nWarehouses = Integer.parseInt(sCurrentLine);
+            Warehouse[] warehouses = parseWarehouses(nWarehouses, weights);
+            System.out.println("End Parse");
+            /*
             while ((sCurrentLine != null && !sCurrentLine.contains("]"))) {
 
                 switch(sCurrentLine){
                     case "OBJECTS[":{
                         System.out.println("\"OBJECTS\" detected, parsing...");
-                        readObjects();
                         break;
                     }
                 }
                 sCurrentLine = reader.readLine();
                 line++;
             }
+            */
 
         }catch(StringIndexOutOfBoundsException e){
             System.err.println("Error occured at line " + line);
@@ -55,5 +79,56 @@ public class TextFileReader {
                 ex.printStackTrace();
             }
         }
+    }
+
+    private int[] parseWeights() {
+        String[] sCurrLineSplit;
+        try {
+            sCurrentLine = reader.readLine();
+            line++;
+            //Read First Line
+            int nProducts = Integer.parseInt(sCurrentLine);
+            sCurrentLine = reader.readLine();
+            line++;
+            sCurrLineSplit = sCurrentLine.split(" ");
+        }catch(Exception e){
+            System.err.println("Parse Exception");
+            sCurrLineSplit = new String[0];
+        }
+
+        int[] weights = new int[sCurrLineSplit.length];
+        for(int i=0; i<sCurrLineSplit.length; i++){
+            weights[i]=(Integer.parseInt(sCurrLineSplit[i]));
+        }
+        return weights;
+    }
+
+    private Warehouse[] parseWarehouses(int nWarehouses, int[] weights){
+        //public Warehouse(int locX, int locY, int numProductTypes)
+        Warehouse[] warehouses = new Warehouse[nWarehouses];
+        for(int i=0; i<nWarehouses; i++){
+            try {
+                sCurrentLine = reader.readLine();
+                line++;
+                //Read First Line
+                String sCurrLineSplit[] = sCurrentLine.split(" ");
+                warehouses[i] = new Warehouse(Integer.parseInt(sCurrLineSplit[0]), Integer.parseInt(sCurrLineSplit[1]), weights.length);
+
+                sCurrentLine = reader.readLine();
+                line++;
+                String sCurrLineSplit2[] = sCurrentLine.split(" ");
+                List<Product> list = new ArrayList<Product>();
+                for(int j=0; j<sCurrLineSplit2.length; j++){
+                    for(int k=0; k<Integer.parseInt(sCurrLineSplit2[j]); k++){
+                        list.add(new Product(j, weights[j]));
+                    }
+                }
+                warehouses[i].addProducts(list);
+
+            }catch(Exception e){
+                warehouses = new Warehouse[0];
+            }
+        }
+        return warehouses;
     }
 }

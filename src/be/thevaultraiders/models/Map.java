b0869f53bcbe02d318f667a2e7531936df121659
Thomas Huybrechts <huybthomas@gmail.com>
2016-02-11 19:43:50 +0100

    Added drones and warehouses to map

--- src/be/thevaultraiders/models/Map.java
+++ src/be/thevaultraiders/models/Map.java
@@ -1,16 +1,23 @@
 package be.thevaultraiders.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Thomas on 11/02/2016.
  */
 public class Map
 {
     private int sizeX, sizeY;
+    private List<Drone> drones;
+    private List<Warehouse> warehouses;
 
     public Map(int sizeX, int sizeY)
     {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
+        this.drones = new ArrayList<Drone>();
+        this.warehouses = new ArrayList<Warehouse>();
     }
 
     public int getSizeX()
@@ -31,5 +38,39 @@ public class Map
     public void setSizeY(int sizeY)
     {
         this.sizeY = sizeY;
+    }
+
+    public void addDrone(Drone drone)
+    {
+        drones.add(drone);
+    }
+
+    public void addWarehouse(Warehouse warehouse)
+    {
+        warehouses.add(warehouse);
+    }
+
+    public Drone getDrone(int number)
+    {
+        if(number > 0 && number < drones.size())
+        {
+            return drones.get(number);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Warehouse getWarehouse(int number)
+    {
+        if(number > 0 && number < warehouses.size())
+        {
+            return warehouses.get(number);
+        }
+        else
+        {
+            return null;
+        }
     }
 }

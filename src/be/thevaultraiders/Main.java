05062ef559b265c3f232fa5cf2e5711ac0b35e7a
Thomas Huybrechts <huybthomas@gmail.com>
2016-02-11 14:49:06 +0100

    Project start

---
+++ src/be/thevaultraiders/Main.java
@@ -0,0 +1,10 @@
+package be.thevaultraiders;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Google Hashcode 2016");
+        System.out.println("The Vault Raiders - 2016");
+    }
+}

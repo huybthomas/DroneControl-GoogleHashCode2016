1ba7fb2e2f3f678d1015dd505ad9609d74e2ffe4
Thomas Huybrechts <huybthomas@gmail.com>
2016-02-11 19:02:20 +0100

    Map class added Drone getter en setters

--- src/be/thevaultraiders/models/Drone.java
+++ src/be/thevaultraiders/models/Drone.java
@@ -3,5 +3,45 @@ package be.thevaultraiders.models;
 /**
  * Created by Thomas on 11/02/2016.
  */
-public class Drone {
+public class Drone
+{
+    private int maxPayload;
+    private int locX, locY;
+
+    public Drone(int maxPayload, int locX, int locY)
+    {
+        this.maxPayload = maxPayload;
+        this.locX = locX;
+        this.locY = locY;
+    }
+
+    public int getMaxPayload()
+    {
+        return maxPayload;
+    }
+
+    public void setMaxPayload(int maxPayload)
+    {
+        this.maxPayload = maxPayload;
+    }
+
+    public int getLocX()
+    {
+        return locX;
+    }
+
+    public void setLocX(int locX)
+    {
+        this.locX = locX;
+    }
+
+    public int getLocY()
+    {
+        return locY;
+    }
+
+    public void setLocY(int locY)
+    {
+        this.locY = locY;
+    }
 }
---
+++ src/be/thevaultraiders/models/Map.java
@@ -0,0 +1,12 @@
+package be.thevaultraiders.models;
+
+/**
+ * Created by Thomas on 11/02/2016.
+ */
+public class Map
+{
+    public Map()
+    {
+
+    }
+}

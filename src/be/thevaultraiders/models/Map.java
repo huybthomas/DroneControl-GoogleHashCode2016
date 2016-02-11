87160a43f9225a20b0c54f7de3be5c669a1973ca
Thomas Huybrechts <huybthomas@gmail.com>
2016-02-11 19:03:23 +0100

    Map getter en setters

--- src/be/thevaultraiders/models/Map.java
+++ src/be/thevaultraiders/models/Map.java
@@ -5,8 +5,31 @@ package be.thevaultraiders.models;
  */
 public class Map
 {
-    public Map()
-    {
+    private int sizeX, sizeY;
 
+    public Map(int sizeX, int sizeY)
+    {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+    }
+
+    public int getSizeX()
+    {
+        return sizeX;
+    }
+
+    public void setSizeX(int sizeX)
+    {
+        this.sizeX = sizeX;
+    }
+
+    public int getSizeY()
+    {
+        return sizeY;
+    }
+
+    public void setSizeY(int sizeY)
+    {
+        this.sizeY = sizeY;
     }
 }
